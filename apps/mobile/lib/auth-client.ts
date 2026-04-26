500e459dbcb2180bbfebdcfb6599687866917d32
TomWilkRavoux <tomwilkwilk@gmail.com>
2026-04-26 17:06:22 +0200

    chore: modification auth-client

--- apps/mobile/lib/auth-client.ts
+++ apps/mobile/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from 'better-auth/react';
 
 export const authClient = createAuthClient({
-  baseURL: 'http://192.168.1.14:3001',
+  baseURL: 'http://192.168.1.129:3001', //En local donc modifier avec son ip sur windows : ipconfig sur linux : ifconfig
 });
